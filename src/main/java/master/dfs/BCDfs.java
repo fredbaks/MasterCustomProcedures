fe3214066ae3c5c98fa01a4dff120f5745aa55cb
Fredrik Baksaas <fredrik@baksaas.no>
2026-03-05 14:08:06 +0100

    fix visited and correct depth

--- src/main/java/master/dfs/BCDfs.java
+++ src/main/java/master/dfs/BCDfs.java
@@ -6,7 +6,6 @@ import java.util.List;
 
 import org.neo4j.gds.api.Graph;
 import org.neo4j.gds.collections.ha.HugeLongArray;
-import org.neo4j.gds.core.utils.paged.HugeLongArrayStack;
 import org.neo4j.logging.Log;
 
 import com.carrotsearch.hppc.BitSet;
@@ -21,24 +20,19 @@ public class BCDfs {
 
     private ArrayList<HugeLongArray> results;
 
-    private HugeLongArrayStack stack;
     private BitSet visited;
     private HashMap<Long, Long> bar;
 
     public ArrayList<HugeLongArray> startBCDfs() {
 
         log.debug("Started BC-Dfs");
-
         results = new ArrayList<HugeLongArray>();
 
-        stack = HugeLongArrayStack.newStack(graph.nodeCount());
-        stack.push(source);
-
-        visited = new BitSet();
+        visited = new BitSet(graph.nodeCount());
 
         bar = new HashMap<Long, Long>();
 
-        HugeLongArray path = HugeLongArray.newArray(k + 1);
+        HugeLongArray path = HugeLongArray.newArray(1);
 
         computeBcDfs(path, source, 0);
 
@@ -55,29 +49,24 @@ public class BCDfs {
 
     private long computeBcDfs(HugeLongArray oldPath, long current, int hopCount) {
 
-        log.debug("ComputeDfs new depth, counter: " + hopCount);
-
         long F = k + 1;
 
-        HugeLongArray path = oldPath.copyOf(k + 1);
+        HugeLongArray path = oldPath.copyOf(hopCount + 1);
 
-        visited.set(source);
         path.set(hopCount, current);
 
         if (current == target) {
-            log.debug("New path to results: " + path.toString());
             results.add(path.copyOf(hopCount + 1));
             return 0L;
         }
 
-        if (hopCount <= k || k == -1) {
-            log.debug("degree of " + current + ": " + graph.degree(current));
+        visited.set(current);
+
+        if (hopCount < k) {
 
             List<Long> neighbors = new ArrayList<Long>();
 
             graph.forEachRelationship(current, (long source, long neighbor) -> {
-
-                log.debug("Continouing with neighbor: " + neighbor + " visited: " + visited.get(neighbor));
 
                 if (!visited.get(neighbor)) {
                     neighbors.add(neighbor);
@@ -87,7 +76,7 @@ public class BCDfs {
             });
 
             for (long neighbor : neighbors) {
-                if (hopCount + 1 + bar.getOrDefault(neighbor, k + 1) <= k) {
+                if (hopCount + 1 + bar.getOrDefault(neighbor, 0L) <= k) {
                     long f = computeBcDfs(path, neighbor, hopCount + 1);
                     if (f != k + 1) {
                         F = Long.min(F, f + 1);
@@ -97,18 +86,17 @@ public class BCDfs {
         }
 
         if (F == k + 1) {
-            bar.put(current, k - hopCount + 1);
+            bar.put(current, k - (hopCount + 1) + 1);
         } else {
             updateBarrier(current, F);
         }
 
-        log.debug("Finished with " + current);
         visited.clear(current);
         return F;
     }
 
     private void updateBarrier(long node, long l) {
-        if (bar.get(node) > l) {
+        if (bar.getOrDefault(node, 0L) > l) {
             bar.put(node, l);
 
             List<Long> neighbours = new ArrayList<Long>();
