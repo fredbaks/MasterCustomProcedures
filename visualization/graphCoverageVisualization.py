1e25210bcc9a3caff5becaf08e396d8dd76081cd
Fredrik Baksaas <fredrik@baksaas.no>
2026-04-24 13:50:21 +0200

    update graph visualization to average over many queries with different source-target and final nodecount

--- visualization/graphCoverageVisualization.py
+++ visualization/graphCoverageVisualization.py
@@ -24,52 +24,58 @@ import os
 import matplotlib.pyplot as plt
 import itertools
 
+COMMENT_SIZE = 11
+HEADER_SIZE = COMMENT_SIZE + 1
+STEP_COUNT = 1001
+PERCENT_STEP_LIST = [x*0.1 for x in range(STEP_COUNT)]
+
 def parse_metadata(filepath):
     metadata = {}
     with open(filepath, "r", encoding="utf-8") as file:
-        for line in itertools.islice(file, 8):
+        for line in itertools.islice(file, COMMENT_SIZE):
             content = line[1:]
             key, _, value = content.partition(":")
             metadata[key] = value.strip()
     return metadata
 
 
-def parse_rows(filepath):
+def parse_rows(filepath) -> list[tuple[str, int]]:
     rows = []
     with open(filepath, "r", encoding="utf-8") as file:
-        for line in itertools.islice(file, 9, None):
+        for line in itertools.islice(file, HEADER_SIZE, None):
             parts = line.split(",")
             rows.append((int(parts[0]), int(parts[1])))
     return rows
 
 
-def required_meta_int(meta, key, filepath):
-    if key not in meta:
-        sys.exit(f"ERROR: '{key}' not found in metadata of {filepath}")
-    try:
-        return int(meta[key])
-    except ValueError:
-        sys.exit(f"ERROR: '{key}' is not an integer in {filepath}  (got '{meta[key]}')")
+def  build_plot_data(rows : list[tuple[str, int]], node_count: int, start_time: int, end_time: int) -> list[float]:
 
+    end_ms = (end_time - start_time) / 1_000_000
 
-def build_plot_data(parsed, node_count, start_time, end_time):
-    rows = parsed["rows"]
+    #Use step size of 0.1
+    x_axis_ms = [-1.0] * STEP_COUNT
 
-    x_axis_ms = [0.0]
-    y_axis_percent = [0.0]
+    #Start at 0%
+    x_axis_ms[0] = 0.0
+    x_axis_ms[STEP_COUNT-1] = end_ms
 
     for index, (_, discovered_at) in enumerate(rows, start=1):
         elapsed_ms = (discovered_at - start_time) / 1_000_000
-        percent = (index / node_count) * 100.0
-        x_axis_ms.append(elapsed_ms)
-        y_axis_percent.append(percent)
+        percent = round((index / node_count) * 100.0, 1)
+        x_axis_ms[int(percent*10)] = elapsed_ms
 
-    end_ms = (end_time - start_time) / 1_000_000
-    if x_axis_ms[-1] < end_ms:
-        x_axis_ms.append(end_ms)
-        y_axis_percent.append(100.0)
+    last_ms_found = end_ms
+    filled_x_axis_ms = [-1.0] * STEP_COUNT
+    for index, elapsed_ms in reversed(list(enumerate(x_axis_ms))):
+        if elapsed_ms == -1.0:
+            filled_x_axis_ms[index] = last_ms_found
+        else:
+            filled_x_axis_ms[index] = elapsed_ms
+            last_ms_found = elapsed_ms
 
-    return x_axis_ms, y_axis_percent
+    filled_x_axis_ms[0] = 0.0
+
+    return filled_x_axis_ms
 
 def main():
     parser = argparse.ArgumentParser(
@@ -97,52 +103,79 @@ def main():
     if not csv_filepaths:
         sys.exit(f"ERROR: No CSV files found in '{args.folder}'")
 
-    accepted_meta = []
+    accepted_meta : dict[str, list[tuple[str,dict[str, str]]]] = {}
+    query_coverages : dict[str, list[list[float]]] = {}
     for filepath in csv_filepaths:
         meta = parse_metadata(filepath)
 
-        file_hoplimit = int(meta.get("HopLimit"))
+        file_hoplimit = int(meta.get("HopLimit", -1))
         if file_hoplimit != args.hoplimit:
             continue
 
-        accepted_meta.append((filepath, meta))
+        #TODO: what todo with timedOut: true?
+        if meta["timedOut"] == 'true':
+            continue
+
+        key = f"{meta["SourceNode"]}-{meta["TargetNode"]}"
+
+        referenceMetas : list[tuple[str, dict[str,str]]] = accepted_meta.setdefault(key, [])
+
+        if (len(referenceMetas) != 0):
+            reference_node_count = referenceMetas[0][1]["NodeCount"]
+            node_count = meta["NodeCount"]
+
+            if (reference_node_count != node_count):
+                f"Error: missmatch between runs on source-target: {key} between {reference_node_count} and {filepath}"
+                continue
+
+        referenceMetas.append((filepath, meta))
+
+        rows = parse_rows(filepath)
+
+        x_axis_ms = build_plot_data(rows, int(meta["NodeCount"]), int(meta["StartTime"]), int(meta["EndTime"]))
+
+        query_coverages.setdefault(meta["Algorithm"], []).append(x_axis_ms)
+        
 
     if not accepted_meta:
         sys.exit(
             f"ERROR: No CSV files with HopLimit={args.hoplimit} found in '{args.folder}'"
         )
 
-    ref_path, reference = accepted_meta[0]
 
-    for filepath, meta in accepted_meta[1:]:
-            if meta.get("NodeCount") != reference.get("NodeCount"):
-                sys.exit(
-                    f"ERROR: Inconsistent 'NodeCount' between files.\n"
-                    f"  {ref_path}: {reference.get("NodeCount")}\n"
-                    f"  {filepath}: {meta.get("NodeCount")}"
-                )
+    algorithm_coverages_avg_elapsed_ms : dict[str, list[float]] = {}
 
-    graph_name = reference.get("GraphName")
-    source_node = reference.get("SourceNode")
-    target_node = reference.get("TargetNode")
-    node_count = int(reference.get("NodeCount"))
-    hop_limit = int(reference.get("HopLimit"))
+    for algorithm, coverages in query_coverages.items():
+
+        coverage_avg_elapsed_ms = [0.0] * STEP_COUNT
+        
+        query_count = len(coverages)
+
+        for coverage in coverages:
+            for index, elapsed_ms in enumerate(coverage):
+                coverage_avg_elapsed_ms[index] += elapsed_ms
+
+        for index, sum_coverage in enumerate(coverage_avg_elapsed_ms):
+            coverage_avg_elapsed_ms[index] = sum_coverage/query_count
+
+        algorithm_coverages_avg_elapsed_ms[algorithm] = coverage_avg_elapsed_ms
+
+
+    meta = list(accepted_meta.items())[0][1][0][1]
+    graph_name = meta["GraphName"]
+    hop_limit = meta["HopLimit"]
+
 
     _, ax = plt.subplots(figsize=(10, 6))
 
-    for filepath, meta in accepted_meta:
-        start_time = int(meta.get("StartTime"))
-        end_time = int(meta.get("EndTime"))
-        algorithm = meta.get("Algorithm")
-
-        rows = parse_rows(filepath)
-        x_axis_ms, y_axis_percent = build_plot_data({"rows": rows}, node_count, start_time, end_time)
-        ax.step(x_axis_ms, y_axis_percent, where="post", label=algorithm, linewidth=2)
+    for algorithm, coverage_avg_elapsed_ms in algorithm_coverages_avg_elapsed_ms.items():
+        print(algorithm, coverage_avg_elapsed_ms[0])
+        ax.step(coverage_avg_elapsed_ms, PERCENT_STEP_LIST, where="post", label=algorithm, linewidth=2)
 
     ax.set_xlabel("Time elapsed (ms)", fontsize=12)
     ax.set_ylabel("Graph coverage (%)", fontsize=12)
     ax.set_title(
-        f"{graph_name}  —  k={args.hoplimit}  |  {source_node} to {target_node}",
+        f"{graph_name}  —  k={args.hoplimit}",
         fontsize=13,
     )
     ax.set_ylim(0, 105)
@@ -150,7 +183,7 @@ def main():
     ax.grid(True, linestyle="--", alpha=0.5)
     plt.tight_layout()
 
-    outputName = f"plots/{graph_name}-k{hop_limit}-source{source_node}-target{target_node}"
+    outputName = f"plots/{graph_name}-k{hop_limit}"
 
     os.makedirs(os.path.dirname(outputName), exist_ok=True)
     plt.savefig(outputName, dpi=150)
