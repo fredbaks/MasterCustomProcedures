e7580a5b5fc8feab1917d7e4c11ccbc7ee9af92d
Fredrik Baksaas <fredrik@baksaas.no>
2026-06-17 15:56:33 +0200

    reduce to 6 threads

--- src/main/java/master/dataHandling/ExperimentHandler.java
+++ src/main/java/master/dataHandling/ExperimentHandler.java
@@ -312,7 +312,7 @@ public class ExperimentHandler {
             }
         }
 
-        ExecutorService executor = Executors.newFixedThreadPool(8);
+        ExecutorService executor = Executors.newFixedThreadPool(6);
 
         try {
             executor.invokeAll(tasks);
